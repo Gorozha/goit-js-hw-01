bc149307864745b5c999cba91e7c52a16b8e82bc
Gorozha <67167342+Gorozha@users.noreply.github.com>
2020-09-23 22:33:01 +0300

    h/w1 23.09.20

---
+++ script.js/js1.js
@@ -0,0 +1,37 @@
+//var x = 5;
+//var y = 2;
+//var z = x + y;
+//document.getElementById('demo').innerHTML = z;
+
+//const myBirthday = 1975;
+//console.log(myBirthday);
+
+//var txt1 = 'John';
+//var txt2 = 'Doe';
+//document.getElementById('demo').innerHTML = txt1 + ' ' + txt2;
+
+//txt1 = 'What a very ';
+//txt1 += 'nice day';
+//document.getElementById('demo').innerHTML = txt1;
+
+//console.log(Number.parseInt('12uy56QX6')); //Методы преобразуют строку символ за символом, пока это возможно. При возникновении ошибки возвращается число, которое получилось.
+//console.log(Number.parseFloat('23.45pg'));
+
+//const validNumber = Number('51');
+//console.log(Number.isNaN(validNumber)); //Проверка на число,false — если значение val это не NaN
+
+//console.log(0.1 + 0.2 === 0.3); //Сложение чисел с плавающей точкой
+//console.log(0.1 + 0.2);
+
+//console.log(Math.pow(5, 5)); // Math.pow(base, exponent) - возведение в степень
+
+//console.log(Math.random() * (10 - 1) + 1);  // случайное число от 1 до 10
+
+//const message = 'Mango ' + 'is' + ' fruit.';
+//console.log(message);
+
+//const message = 'Happy New Year, Mr. Broun!';
+
+//console.log(message.length); //length - свойство, хранит длину строки
+//console.log(message.toLowerCase()); // все в нижнем регистре
+//console.log(message.toUpperCase()); // все в верхнем регистре
---
+++ script.js/task1.js
@@ -0,0 +1,6 @@
+let price;
+const name = 'Protective field generator';
+
+console.log(`Выбран ${name}, цена за штуку ${(price = 1000)} кредитов.`);
+
+console.log(`Выбран ${name},цена за штуку ${(price = 2000)} кредитов.`);
