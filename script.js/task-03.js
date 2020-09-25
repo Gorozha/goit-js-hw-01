946b108bdadff5138536c2169d33cfaf89b06445
Gorozha <67167342+Gorozha@users.noreply.github.com>
2020-09-25 21:11:03 +0300

    h/w 01-4

--- script.js/task-03.js
+++ script.js/task-03.js
@@ -1,21 +1,17 @@
-const userPassword = 'jqueryismyjam';
-
+//let userPassword = prompt('Введите пароль!');
 const ADMIN_PASSWORD = 'jqueryismyjam';
-
 const CANCELED_BY_USER = 'Отменено пользователем!';
 const ACCESS_IS_ALLOWED = 'Добро пожаловать!';
 const ACCESS_DENIED = 'Доступ запрещен, неверный пароль!';
 let message;
+let userPassword = prompt('Введите пароль!');
 
-// Write code under this line
-if ((userPassword = 'jqueryismyjam')) {
+if (ADMIN_PASSWORD === userPassword) {
   message = ACCESS_IS_ALLOWED;
-} else if ((userPassword = 'null')) {
+} else if (userPassword === null) {
   message = CANCELED_BY_USER;
-} else if ((userPassword = '123')) {
+} else if (ADMIN_PASSWORD !== userPassword) {
   message = ACCESS_DENIED;
 }
-console.log('Добро пожаловать!');
-console.log('Отменено пользователем!');
-console.log('Доступ запрещен, неверный пароль!');
-//если userPassword равно 'jqueryismyjam'
+console.log(message);
+alert(message);
